bc445c5feae7c59f44d5812b17c39d8e0f115f69
Grzegorz Pach <grzegorz.pach@swmansion.com>
2024-07-09 13:40:48 +0200

    Cleanup layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,15 @@
-import { ScrollView, StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import { SafeView } from '@/components/layout/SafeView';
 import { useWeatherContext } from '@/providers/WeatherProvider';
 import { CurrentWeather } from '@/components/CurrentWeather/CurrentWeather';
+import { WeatherTable } from '@/components/WeatherTable/WeatherTable';
 
 export default function HomeScreen() {
-  const { weather } = useWeatherContext();
+  const { weather, isLoading } = useWeatherContext();
 
   return (
     <SafeView>
-      <CurrentWeather weather={weather[0]} />
-      <ScrollView>
-        {weather.slice(1).map((entry) => (
-          <ThemedText>
-            {entry.time.format('DD/MM/YYYY HH:mm')} {entry.temperature}
-          </ThemedText>
-        ))}
-      </ScrollView>
+      <CurrentWeather weather={weather[0]?.entries[0]} isLoading={isLoading} />
+      <WeatherTable weatherData={weather} />
     </SafeView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
